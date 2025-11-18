ee5af3f22a1fd5f2dd8bebb94256dffa5c1ede7f
AndrewEl131 <andyelizbarashvilii@gmail.com>
2025-11-18 21:55:35 +0400

    Update useProjectStore.js

--- src/store/useProjectStore.js
+++ src/store/useProjectStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import useUserStore from "./useUserStore";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://task-manager-mern-appu.onrender.com";
 
 const useProjectStore = create((set, get) => ({
   projects: [],
